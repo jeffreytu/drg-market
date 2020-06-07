54ce5a1e1e749996d268692177da65e7971c11e8
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-06-06 23:25:08 -0700

    user register add to customer group

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
 
 # Create your views here.
 from django.urls import reverse_lazy
@@ -10,3 +11,8 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+    def form_valid(self, form):
+        group = Group.objects.get(name='customer')
+        self.object.groups.add(group)
+        return super(SignUpView, self).form_valid(form)
