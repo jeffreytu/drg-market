5b0f7ff9a33251766e44db96333b445f023922bf
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-07-02 19:39:29 -0700

    fix user address instance update

--- store/views.py
+++ store/views.py
@@ -129,32 +129,32 @@ def buyListing(request, listing_id):
 
     try:
         user_address = UserAddress.objects.get(user__id=user.id)
+        form_address = ChangeAddressForm(instance=user_address)
     except UserAddress.DoesNotExist:
         user_address = None
         form_address = ChangeAddressForm()
 
+    form = TransactionForm(initial={
+    'buyer': buyer,
+    'listing': listing,
+    'seller': seller,
+    'status': 1
+        })
+
     if request.method == "POST":
         if 'set_useraddress' in request.POST:
-            form_address = ChangeAddressForm(data=request.POST)
+            form_address = ChangeAddressForm(data=request.POST, instance=user_address)
             if form_address.is_valid():
                 # To save the object, save the instance object, not the form.
-                instance = form_address.save(commit=False)
-                instance.user = user
-                instance.save()
+                obj = form_address.save(commit=False)
+                obj.user = user
+                obj.save()
                 return redirect('buy-listing', listing_id=listing_id)
         elif 'transaction' in request.POST:
             form = TransactionForm(data=request.POST, status=listing.status, address=user_address)
             if form.is_valid():
                 form.save()
                 return redirect('buy-transaction', listing_id=listing_id)
-    else:
-        form = TransactionForm(initial={
-        'buyer': buyer,
-        'listing': listing,
-        'seller': seller,
-        'status': 1
-            })
-        form_address = ChangeAddressForm(instance=user_address)
 
     context = {
         'listing': listing,
