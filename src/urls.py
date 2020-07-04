0c42dc84b9ddd14a290f04f49823e45e7372fc1f
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-07-04 15:54:54 -0700

    install django debug toolbar

--- src/urls.py
+++ src/urls.py
@@ -10,3 +10,9 @@ urlpatterns = [
     path('', include('store.urls')),
     path('users/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
