8a8f691c99a6f4c44106c0fcb7b9db423ecb8ca0
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-07-08 23:27:32 -0700

    replace tree navigation categories

--- store/context_processors.py
+++ store/context_processors.py
@@ -2,10 +2,14 @@ from .models import Category
 
 def menuCategories(request):
 
-    menu_categories = Category.objects.all()
+    menu_categories = Category.objects.select_related('parent').all()
+    iphones = menu_categories.get(slug='apple-iphone').get_children()
+
+    print(iphones)
 
     return {
 
-        'menu_categories': menu_categories
+        'menu_categories': menu_categories,
+        'iphones': iphones,
         
     }
